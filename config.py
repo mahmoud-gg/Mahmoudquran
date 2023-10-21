99a067157d7fa9013370bfac0c2bb065b5aeeb4a
mahmoud-gg <136716364+mahmoud-gg@users.noreply.github.com>
2023-10-21 23:39:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID"))
 
 PING_IMG = getenv("PING_IMG", "https://graph.org/file/d7f33299ed8c60ad82721.mp4")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/69bafebb4fab335f2acc6.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/66583155ffd41860a871e.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/YY5Y8")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1095638828").split()))
 
 
-FAILED = "https://telegra.ph/file/69bafebb4fab335f2acc6.jpg"
+FAILED = "https://telegra.ph/file/66583155ffd41860a871e.jpg"
